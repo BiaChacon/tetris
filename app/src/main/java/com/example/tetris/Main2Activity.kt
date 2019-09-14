8644e138659fc2f56f910eba1a5b3ae64e7b02f5
biachacon <anabheatrizchacon@outlook.com>
2019-09-13 23:50:34 -0300

    update tela

---
+++ app/src/main/java/com/example/tetris/Main2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.tetris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.ufrn.eaj.tads.gametetris.R
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
